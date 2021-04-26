a8a1d866cd4aa55ac28db5466971dd67d7632cfc
F-betanzos <82618274+F-betanzos@users.noreply.github.com>
2021-04-26 12:35:31 -0500

    Update Programa3.c

--- Programa3.c
+++ Programa3.c
@@ -1,19 +1,20 @@
-# incluye  < stdio.h >
-int  main ()
+
+# incluye <stdio.h>
+int   main ()
 {
-     / * Cesar Méndez
+     / * Gonzalez Betanzos Fabiola
     Equipo 1
     21 abr 21
-    Programa 3. Pide dos valores reales, calcula promedio y muestra resultado
+    Programa 3 . Pide dos valores reales, calcula promedio y muestra resultado
     * /
-    cebador flotante , segundo, tercero;
-    printf ( " \ n \ n Programa 3 " );
-    printf ( " \ n \ n Saca el promedio de valores reales " );
-    printf ( " \ n \ n Dame el primer valor real " );
-    scanf ( " % f " , & cebador); // Permite capturar un valor real y lo guarda en variable primer
-    printf ( " \ n \ n Dame el segundo valor real " );
-    scanf ( " % f " , & segundo); // Permite capturar un valor real y lo guarda en variable primer
+    cebador flotante, segundo, tercero;
+    printf ( "  \ n \ n Programa 3 " );
+    printf ( "  \ n \ n Saca el promedio de valores reales " );
+    printf ( "  \ n \ n Dame el primer valor real " );
+    scanf ( "  % f  " , & cebador); // Permite capturar un valor real y lo guarda en variable primer
+    printf ( "  \ n \ n Dame el segundo valor real " );
+    scanf ( "  % f  " , & segundo); // Permite capturar un valor real y lo guarda en variable primer
     tercero = (cebador + segundo) / 2 ;
-    printf ( " \ n El promedio es % f " , tercero);
-    return  0 ;
+    printf ( "  \ n El promedio es % f  " , tercero);
+    return   0 ;
 }
