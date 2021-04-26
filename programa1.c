aeeb725fc70244cdf0be21c0ed0f090cf0b6d4d8
F-betanzos <82618274+F-betanzos@users.noreply.github.com>
2021-04-26 12:48:24 -0500

    Update programa1.c

--- programa1.c
+++ programa1.c
@@ -1,12 +1,12 @@
-# incluye <stdio.h>
+#include  <stdio.h>
 
-int   main () {
-    / * Gonzalez Betanzos Fabiola
-    Equipo 1
-    21 abr 21
+int main () {
+    /* Gonzalez Betanzos Fabiola
+    Equipo "Alfa Buena Maravilla Onda Dinamita Escuadron Lobo"
+    23 abr 21
     Programa 1
-    * /
+    */
 
-   printf ( " Hola mundo " );
-   return   0 ;
+    printf ("Hola Mundo")
+    return 0;
 }
