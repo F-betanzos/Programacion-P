7e7a175313de975182883c7c8a8f70c7c08b8bba
F-betanzos <82618274+F-betanzos@users.noreply.github.com>
2021-04-26 12:56:29 -0500

    Create Trapeciovolumen.c

---
+++ Trapeciovolumen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    /* Gonzalez Betanzos Fabiola
+    Esquipo "Alfa Buena Maravilla Escuadron Lobo"
+    23 abr 21
+    Programa 6. Calcular el volumen de un trapecio
+    */
+
+    int opcion;
+    float Base, base, altura;
+    printf("Dame el valor de la Base:");
+    scanf("%f" , &base);
+    printf("Dame el valor de la altura:");
+    scanf("%f" . &altura);
+    printf("Dame el valor de la base:");
+    scanf("%f" . &base);
+    volumen = (&base + &Base) /2
+    printf ("El valor del volumen es: "%f", volumen");
+    return 0;
+}
