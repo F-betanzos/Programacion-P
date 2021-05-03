a5cb91f8e99d51ca8beb8d5b17dc2f99d4b7a0ff
F-betanzos <82618274+F-betanzos@users.noreply.github.com>
2021-05-03 16:18:52 -0500

    Update raizcuadrada.c

--- raizcuadrada.c
+++ raizcuadrada.c
@@ -1,13 +1,12 @@
 #include <math.h>
 #include <stdio.h>
-int main(int argc, char const *argv[])
+int main(int argc, char const *argv[]){
 	  /* Gonzalez Betanzos Fabiola
     Esquipo "Alfa Buena Maravilla Escuadron Lobo"
     30 abr 21
     Programa 6.
     */
 
-{
 	double numero; // Aquí vamos a almacenar lo que el usuario introduzca
 	printf("Escribe un numero y te dire su raiz cuadrada\n");
 	scanf("%lf", &numero); // Escanear lo que el usuario ingresa
