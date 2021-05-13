a58a89558767bb4008195bfa0fddfa6637f66f88
F-betanzos <82618274+F-betanzos@users.noreply.github.com>
2021-05-12 21:06:56 -0500

    Create Exc1.c

---
+++ Exc1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+   /*FABIOLA GONZALEZ BETANZOS 
+	EQUIPO ALFA BUENA MARAVILLA
+	12 de mayo 
+	Haz que coloque tu nombre en pantalla
+    */
+
+    float primer, segundo, multiplica, division;
+    printf ("\n\nFabiola Gonzalez Betanzos");
+    printf ("\n\n Saca el promedio de valores reales");
+    printf ("\n\n Dame el primer valor real ");
+    scanf ("%f",&primer);
+    printf ("\n\n Dame el segundo valor real ");
+    scanf ("%f",&segundo);
+   multiplica = (primer * segundo);
+    printf ("\n El promedio es %f",division);
+    return 0;
+	}
