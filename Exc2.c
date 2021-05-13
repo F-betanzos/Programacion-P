fef14ecce46c11345b9406fbb70e02927e58ea15
F-betanzos <82618274+F-betanzos@users.noreply.github.com>
2021-05-12 21:08:40 -0500

    Update and rename Ex2.c to Exc2.c

--- Exc2.c
+++ Exc2.c
@@ -3,15 +3,14 @@ int main(){
    /*FABIOLA GONZALEZ BETANZOS 
 	EQUIPO ALFA BUENA MARAVILLA
 	12 de mayo 
-	Haz que coloque tu nombre en pantalla
-    */
-
-    int a, b, c;
+	*/
+	
+    int  a, b, c;
     printf ("\n \n \nFabiola Gonzalez Betanzos \n");
-    printf ("Suma de numeros enteros \n");
+    printf ("Suma de números enteros \n");
     printf ("\n \nDame el valor de a: ");
     scanf  ("%d",&b);
     c == a + b;
-    printf ("\nEl resultado es %d",c);
+    printf ("\n El resultado es %d",c);
     return 0;
 	}
