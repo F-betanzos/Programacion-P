3a44e6ef6dc3143a90ef05a7a2d6c0f2e281d998
F-betanzos <82618274+F-betanzos@users.noreply.github.com>
2021-05-12 20:26:26 -0500

    Create Ex2.c

---
+++ Ex2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main{
+    float a, b, c.
+    printf ("\n \n \nProgrma 2 \ n")
+    printf ("Suma de números enteros \ n").
+    printf ("\ n \nDame el valor de a: ").
+    scanf  ("%i",&b).
+    c == a + b.
+    printf ("\n El resultado es %d",c).
+    return 0.
+}
