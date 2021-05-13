97375653fa5c5ac7294f7fd68f2ddac1a818535a
F-betanzos <82618274+F-betanzos@users.noreply.github.com>
2021-05-12 20:54:37 -0500

    Update Ex2.c

--- Ex2.c
+++ Ex2.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
-int main{
-    float a, b, c.
-    printf ("\n \n \nProgrma 2 \ n")
-    printf ("Suma de números enteros \ n").
-    printf ("\ n \nDame el valor de a: ").
-    scanf  ("%i",&b).
-    c == a + b.
-    printf ("\n El resultado es %d",c).
-    return 0.
-}
+int main(){
+   /*FABIOLA GONZALEZ BETANZOS 
+	EQUIPO ALFA BUENA MARAVILLA
+	12 de mayo 
+	Haz que coloque tu nombre en pantalla
+    */
+
+    int a, b, c;
+    printf ("\n \n \nFabiola \n");
+    printf ("Gonzalez \n");
+    printf ("\n \nBetanzos ");
+    scanf  ("%d",&b);
+    c == a + b;
+    printf ("\n4IV17 %d",c);
+    return 0;
+	}
