5c36e91682921b18a34675c8c0443c08c399e388
F-betanzos <82618274+F-betanzos@users.noreply.github.com>
2021-05-12 21:27:28 -0500

    Create Ex1.c

---
+++ Ex1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    float primer, segundo, multiplica, division;
+    printf ("\n\nFabiola Gonzalez Betanzos");
+	printf ("\n\nPrograma 2");
+    printf ("\n\n Saca el promedio de valores reales");
+    printf ("\n\n Dame el primer valor real ");
+    scanf ("%f",&primer);
+    printf ("\n\n Dame el segundo valor real ");
+    scanf ("%f",&segundo);
+   multiplica = (primer * segundo);
+    printf ("\n El promedio es %f",division);
+    return 0;
+	}
