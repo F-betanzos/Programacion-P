d00821a69a8d318a61052c02be62562edcda1e1e
F-betanzos <82618274+F-betanzos@users.noreply.github.com>
2021-05-12 21:25:07 -0500

    Create Ex2.c

---
+++ Ex2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int  a, b, c;
+    printf ("\n \n \nFabiola Gonzalez Betanzos \n");
+    printf ("\n \n \nPrograma 2 \n");
+    printf ("Suma de números enteros \n");
+    printf ("\n \nDame el valor de a: ");
+    scanf  ("%d",&b);
+    c == a + b;
+    printf ("\n El resultado es %d",c);
+    return 0;
+}
